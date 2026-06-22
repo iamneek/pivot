0720b5f2c88add226b07478cc82e284f308af00b
Coolbit <neeoub@gmail.com>
2026-06-22 11:26:09 +0545

    Feat: Run method for hub added

--- server/hub.go
+++ server/hub.go
@@ -1,8 +1,27 @@
 package main
 
+type Broadcast struct {
+	message []byte
+	sender  *Client
+}
+
 type Hub struct {
 	clients    map[*Client]bool
-	broadcast  chan []byte
+	broadcast  chan Broadcast
 	register   chan *Client
 	unregister chan *Client
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				// close(client.send)
+			}
+		}
+	}
 }
